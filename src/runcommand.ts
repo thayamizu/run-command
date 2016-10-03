f1bbe2f1d6157cafac6f21728d0ed3bcaedd9259
thayamizu <t.hayamizu@outlook.com>
2016-10-04 00:31:19 +0900

    add src/runcommand.ts

---
+++ src/runcommand.ts
@@ -0,0 +1,118 @@
+import * as vscode from 'vscode';
+import * as cp from 'child_process';
+var iconv = require('iconv-lite');
+
+export namespace RunCommand
+{
+	/**
+	 * RunCommand
+	 */
+	export class RunCommand {
+		
+		/**
+		 * command configuration
+		 */
+		private _configuration : Configuration;
+
+		private OUTPUT_CHANNEL_NAME : string = "Shell Command Result";
+
+		/**
+		 * constructor
+		 */
+		constructor() {
+			try {
+				this._configuration = new Configuration('run-command');
+			} catch (error) {
+				vscode.window.showErrorMessage(error.message);
+			}
+		}
+
+		/**
+		 * executeShellCommand
+		 * @param parameter : string
+		 */
+		public executeShellCommand(parameter : string) {
+			let path = vscode.workspace.rootPath;
+			let resultDocument = this.createOutputChannel();
+
+			cp.exec(parameter,{cwd:path, env:null}, (error, stdout, stderr) => {
+				if (error) {
+					vscode.window.showErrorMessage(error.message);
+				}
+				if (stdout) {
+					let data = iconv.decode(stdout, this._configuration.encoding);
+					resultDocument.appendLine(data);
+				}
+			});
+		}
+		
+		/**
+		 * executeShellCommandFromSelectedText
+		 * @param parameter : string
+		 */
+		public executeShellComandFromSelectedText(text : string)  {
+			const path = vscode.workspace.rootPath;
+			const resultDocument = this.createOutputChannel();
+
+			cp.exec(text, {cwd:path, env:null}, (error, stdout, stderr)=>{
+				if (error) {
+					vscode.window.showErrorMessage(error.message);            
+				}
+				if (stdout) {
+					const data = iconv.decode(stdout, this._configuration);
+					resultDocument.append(data);
+				}
+			});
+		}
+		
+		/**
+		 * createOutputChannel
+		 */
+		private createOutputChannel() : vscode.OutputChannel {
+			let resultDocument : vscode.OutputChannel = vscode.window.createOutputChannel(this.OUTPUT_CHANNEL_NAME);
+			resultDocument.show(true);
+			return resultDocument;
+		}
+	}
+
+	/**
+	 * Configuration
+	 */
+	export class Configuration
+	{
+		/**
+		 * constructor
+		 */
+		constructor(parameter : string) {
+			try {
+				let configuration = vscode.workspace.getConfiguration(parameter);
+				this.historySize   = configuration.get<number>('history-size');
+				this.encoding      = configuration.get<string>('encoding');	
+			} catch (error) {
+				vscode.window.showErrorMessage(error.message);
+			}
+		}
+
+		/**
+		 * Encoding
+		 */
+		private _encoding : string;
+		public get encoding() : string {
+			return this._encoding;
+		}
+		public set encoding(v : string) {
+			this._encoding = v;
+		}
+
+		/**
+		 * History Size
+		 */
+		private _historySize : number;
+		public get historySize() : number {
+			return this._historySize;
+		}
+		public set historySize(v : number) {
+			this._historySize = v;
+		}
+	}
+}
